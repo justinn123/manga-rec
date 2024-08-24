05e61b628932ae38d0f1524400e16ee606b844c0
Justin Nguyen <justinn123123123@gmail.com>
2024-08-24 01:21:39 -0400

    added conditional to check whether certain fields are null before getting values, and added error messages

--- scrape.py
+++ scrape.py
@@ -10,10 +10,14 @@ if response.status_code == 200:
 
     soup = BeautifulSoup(webpage, 'html.parser')
     mangas = soup.find_all(class_="col-12 col-lg-6 p-3 text")
+    if not mangas:
+        print("There was an issue retrieving data for mangas")
+        exit(0)
+        
     count = 0
 
     for manga in mangas:
-        if count == 50:
+        if count == 3:
             break
         manga_link = manga.find(class_="col-auto align-self-center series_thumb p-0")
         manga_link = manga_link.find('a')
@@ -31,34 +35,45 @@ if response.status_code == 200:
                 
                 #Get title of manga
                 title = next_soup.find(class_ = "releasestitle tabletitle")
-                print(f"Title: {title.get_text()}")
+                if title:
+                    print(f"Title: {title.get_text()}")
+                else:
+                    print("Could not get title of this manga")
 
                 #Get categories of manga
                 category_list = next_soup.find(class_="tags")
+                print(f"Categories:")
                 if category_list:
                     category_list = category_list.find_all('li')
-                    print(f"Categories:")
                     for item in category_list:
                         print(f"\t{item.get_text()}")
                 else:
-                    continue
+                    print("There was an error getting categories of this manga")
 
                 #Get genre of manga
-                genre = next_soup.find(class_ = "col-6 p-2 text")
-                genre = genre.find_next_sibling('div')
-                genre = genre.find('div')
-                genre = genre.find_next_sibling('div').find_next_sibling('div').find_next_sibling('div')
-                genre = genre.find_all('u')
-                print(f"Genres:")
-                for item in genre[:-1]:
-                    print(f"\t{item.get_text()}")
+                genre = (next_soup.find(class_="col-6 p-2 text")
+                .find_next_sibling('div')
+                .find('div')
+                .find_next_sibling('div')
+                .find_next_sibling('div')
+                .find_next_sibling('div')
+                .find_all('u'))
+                
+                if genre:
+                    print(f"Genres:")
+                    for item in genre[:-1]:
+                        print(f"\t{item.get_text()}")
+                else:
+                    print("Could not get genres of this manga")
 
                 #Get rating of manga
-                rating = next_soup.find('div', string='User Rating')
-                rating = rating.find_next_sibling('div')
-                first_line = rating.get_text().splitlines()[0]
-                rating = first_line.split()[1]
-                print(f"Rating: {rating}")
+                rating = (next_soup.find('div', string='User Rating').find_next_sibling('div'))
+                if rating: 
+                    first_line = rating.get_text().splitlines()[0]
+                    rating = first_line.split()[1]
+                    print(f"Rating: {rating}")
+                else:
+                    print("Could not get rating of this manga")
 
 
             else:
