14e539047449c7f4961e1e3b3775e977e9689c50
Justin Nguyen <justinn123123123@gmail.com>
2024-08-24 18:48:00 -0400

    Updated README and added a loop for different pages of the site since there is a max of 100 manga per page

--- README.md
+++ README.md
@@ -3,6 +3,5 @@ Script that collects data on the best rated manga (> 7/10 rating) and based on m
 a decent rating.
 
 ## Technology 
-The API of the manga website was not very useful as it did not provide the required data for the dataset I was looking for. 
-Because of this, I had to manually scrape the data myself using BeautifulSoup. Currently the scraper gets the manga in order of the Bayesian Average rating
-but I am just using the user rating average listed on the site. 
+The API of the manga website was not very useful as it did not provide the required data for the dataset I was looking for.
+Because of this, I had to manually scrape the data myself using BeautifulSoup.
--- scrape.py
+++ scrape.py
@@ -1,88 +1,90 @@
 from bs4 import BeautifulSoup
 import requests
 
-URL = "https://www.mangaupdates.com/series.html?orderby=rating&perpage=100"
+rating = 10.0
+curr_page = 1
 
-response = requests.get(URL)
+while True:
+    URL = f"https://www.mangaupdates.com/series.html?page={curr_page}&orderby=rating&perpage=5"
 
-if response.status_code == 200:
-    webpage = response.text
+    response = requests.get(URL)
 
-    soup = BeautifulSoup(webpage, 'html.parser')
-    mangas = soup.find_all(class_="col-12 col-lg-6 p-3 text")
-    if not mangas:
-        print("There was an issue retrieving data for mangas")
-        exit(0)
-        
-    count = 0
+    if response.status_code == 200:
+        webpage = response.text
 
-    for manga in mangas:
-        if count == 3:
-            break
-        manga_link = manga.find(class_="col-auto align-self-center series_thumb p-0")
-        manga_link = manga_link.find('a')
+        soup = BeautifulSoup(webpage, 'html.parser')
+        mangas = soup.find_all(class_="col-12 col-lg-6 p-3 text")
+        if not mangas:
+            print("There was an issue retrieving data for mangas")
+            exit(0)
 
-        if manga_link and 'href' in manga_link.attrs:
-            next_url = manga_link['href']
+        count = 0
+
+        for manga in mangas:
             
-            if not next_url.startswith('http'):
-                next_url = requests.compat.urljoin(URL, next_url)
+            divs = manga.find_all(class_="text")
+            rating = divs[3].find('b').get_text()
+            if not rating or float(rating) < 8.8:
+                exit()
             
-            new_response = requests.get(next_url)
+            
+            manga_link = manga.find(class_="col-auto align-self-center series_thumb p-0")
+            manga_link = manga_link.find('a')
 
-            if new_response.status_code == 200:
-                next_soup = BeautifulSoup(new_response.text, 'html.parser')
-                
-                #Get title of manga
-                title = next_soup.find(class_ = "releasestitle tabletitle")
-                if title:
-                    print(f"Title: {title.get_text()}")
-                else:
-                    print("Could not get title of this manga")
+            if manga_link and 'href' in manga_link.attrs:
+                next_url = manga_link['href']
 
-                #Get categories of manga
-                category_list = next_soup.find(class_="tags")
-                print(f"Categories:")
-                if category_list:
-                    category_list = category_list.find_all('li')
-                    for item in category_list:
-                        print(f"\t{item.get_text()}")
-                else:
-                    print("There was an error getting categories of this manga")
+                if not next_url.startswith('http'):
+                    next_url = requests.compat.urljoin(URL, next_url)
 
-                #Get genre of manga
-                genre = (next_soup.find(class_="col-6 p-2 text")
-                .find_next_sibling('div')
-                .find('div')
-                .find_next_sibling('div')
-                .find_next_sibling('div')
-                .find_next_sibling('div')
-                .find_all('u'))
-                
-                if genre:
-                    print(f"Genres:")
-                    for item in genre[:-1]:
-                        print(f"\t{item.get_text()}")
-                else:
-                    print("Could not get genres of this manga")
+                new_response = requests.get(next_url)
 
-                #Get rating of manga
-                rating = (next_soup.find('div', string='User Rating').find_next_sibling('div'))
-                if rating: 
-                    first_line = rating.get_text().splitlines()[0]
-                    rating = first_line.split()[1]
+                if new_response.status_code == 200:
+                    next_soup = BeautifulSoup(new_response.text, 'html.parser')
+
+                    #Get title of manga
+                    title = next_soup.find(class_ = "releasestitle tabletitle")
+                    if title:
+                        print(f"Title: {title.get_text()}")
+                    else:
+                        print("Could not get title of this manga")
+
+                    #Get categories of manga
+                    category_list = next_soup.find(class_="tags")
+                    print(f"Categories:")
+                    if category_list:
+                        category_list = category_list.find_all('li')
+                        for item in category_list:
+                            print(f"\t{item.get_text()}")
+                    else:
+                        print("There was an error getting categories of this manga")
+
+                    #Get genre of manga
+                    genre = (next_soup.find(class_="col-6 p-2 text")
+                    .find_next_sibling('div')
+                    .find('div')
+                    .find_next_sibling('div')
+                    .find_next_sibling('div')
+                    .find_next_sibling('div')
+                    .find_all('u'))
+
+                    if genre:
+                        print(f"Genres:")
+                        for item in genre[:-1]:
+                            print(f"\t{item.get_text()}")
+                    else:
+                        print("Could not get genres of this manga")
+
+                    #Get rating of manga
                     print(f"Rating: {rating}")
                 else:
-                    print("Could not get rating of this manga")
-
-
+                    print(f"Failed to retrieve the page. Status code: {new_response.status_code}")
             else:
-                print(f"Failed to retrieve the page. Status code: {new_response.status_code}")
-        else:
-            print("No link found on the page.")
-        count += 1
-else:
-    print(f"Failed to retrieve website. Status code: {response.status_code}")
+                print("No link found on the page.")
+            count += 1
+    else:
+        print(f"Failed to retrieve website. Status code: {response.status_code}")
+    curr_page += 1
 
 
 
